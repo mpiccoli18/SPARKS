837d727bdaa5d8b81225a46f9a92a9b0b48858ed
Timoothe <timothepitault@gmail.com>
2025-03-28 20:37:47 +0100

    uncommented

--- src/measurement/enrol_server.cpp
+++ src/measurement/enrol_server.cpp
@@ -172,7 +172,7 @@ int main() {
 }
 
 
-auto start = std::chrono::high_resolution_clock::now(); 
+// auto start = std::chrono::high_resolution_clock::now(); 
 // auto end = std::chrono::high_resolution_clock::now();
 // auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 // std::cout << "Execution time for enrolment : " << duration.count() << " microseconds" << std::endl;
